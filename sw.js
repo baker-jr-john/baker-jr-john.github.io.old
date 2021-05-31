a59c580f39bee3493bbd0f7ef9374d49e96e1fb6
John Baker <hello@johnbaker.io>
2021-05-31 12:40:03 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3f0b26f4cedaa9b63d0f.js"
+    "url": "webpack-runtime-7506fe094577e3f80729.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bc68bcc6c53eb4d1a4510ae6fa650d33"
+    "revision": "ea9e3ce13a686ddee01a8c6120353687"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
